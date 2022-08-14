69195b68fc70db0ced8d8a9fe5c74b8c117db28d
Suhel <suhelshaikh954@gmail.com>
2022-08-14 18:16:27 -0500

    codewars

--- python_practice/codewars/alphabet_position.py
+++ python_practice/codewars/alphabet_position.py
@@ -1,4 +1,13 @@
+def alphabet_position(text):
+    text = text.lower()
+    string = ''
 
-
-
+    for i in text:
+        #checking for alphabets only
+        if i.isalpha():
+            #ord gives the ascii value of the lower case letter which should be subtracted by 96 to get order of letter
+            string += str(ord(i) - 96)
+            string += " "
+        
+    return string.strip()   #remove whitespace with strip()
 
