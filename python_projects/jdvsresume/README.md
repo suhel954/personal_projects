b05df40bbd9909cf90052156b37dcf7475fdc914
Suhel <suhelshaikh954@gmail.com>
2022-08-28 07:59:15 -0500

    upto job search page done

--- python_projects/jdvsresume/README.md
+++ python_projects/jdvsresume/README.md
@@ -1,4 +1,7 @@
 Script to automate job search and create a csv of matching keywords
 
-Current Issue: If logged in to linkedin to search for a job, linkedin will do a bot verification.
+Issue #2: Need to find a way to select each job listing from the class: jobs-search__results-list and copy the text of the job description
+
+(Solved)
+Issue #1: If logged in to linkedin to search for a job, linkedin will do a bot verification.
 Proposed Solution: Go to google and search for the jobs in linkedin, look for this website 'https://www.linkedin.com/company/linkedin/jobs'. If directly going to this website then linkedin login is required. Can be bypassed by going through google search. Find text, 'LinkedIn: Jobs'
--- python_projects/jdvsresume/jb_inputs.py
+++ python_projects/jdvsresume/jb_inputs.py
@@ -3,32 +3,50 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def login_linkedin():
-    linkedin_username = input('Please provide linkedin username: ')
-    linkedin_password = input('Please provide linkedin password: ')
+
+    #Inputs
+
+    # linkedin_username = input('Please provide linkedin username: ')
+    # linkedin_password = input('Please provide linkedin password: ')
+    job_name = input('Please input the job name: ')
+    job_location = input('Please provide location: ')
+
     chrome_options = webdriver.ChromeOptions()
     #prevent window from closing
     global driver
     driver = webdriver.Chrome(executable_path=r"D:\Programming\drivers\chromedriver.exe", chrome_options=chrome_options)
     chrome_options.add_argument("--incognito")
-    driver.get('https://www.linkedin.com/')
+    driver.get('https://www.google.com/')
     driver.maximize_window()
     
-    #username field
-    element = driver.find_element(By.ID, 'session_key')
+    #google search field
+    element = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
     element.clear()
-    element.send_keys(linkedin_username)
+    element.send_keys('LinkedIn: Jobs')
+    element.send_keys(Keys.ENTER)
 
-    #password field
-    element = driver.find_element(By.ID, 'session_password')
-    element.clear()
-    element.send_keys(linkedin_password)
-
-    #login button
-    element = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/button')
+    #find the linkedin search page
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, 'Search for the Right Jobs'))
+    )
     element.click()
+
+    #search for the job
+    #job_input
+    element = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/header/nav/section/section[2]/form/section[1]/input')))
+    element.clear()
+    element.send_keys(job_name)
+
+    #location
+    element = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/header/nav/section/section[2]/form/section[2]/input')))
+    element.clear()
+    element.send_keys(job_location)
+    element.send_keys(Keys.ENTER)
 
 login_linkedin()
 
