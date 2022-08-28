a95086f0a4b3b52429b7edbbcc6f6b67b57e11fb
Suhel <suhelshaikh954@gmail.com>
2022-08-28 06:36:44 -0500

    selenium initiated for job script

---
+++ python_projects/jdvsresume/README.md
@@ -0,0 +1,4 @@
+Script to automate job search and create a csv of matching keywords
+
+Current Issue: If logged in to linkedin to search for a job, linkedin will do a bot verification.
+Proposed Solution: Go to google and search for the jobs in linkedin, look for this website 'https://www.linkedin.com/company/linkedin/jobs'. If directly going to this website then linkedin login is required. Can be bypassed by going through google search. Find text, 'LinkedIn: Jobs'
--- python_projects/jdvsresume/jb_inputs.py
+++ python_projects/jdvsresume/jb_inputs.py
@@ -1,9 +1,34 @@
-def job_inputs():
-    job_name = input('Please enter the position you are looking for: ')
-    job_location = input('Please enter the location: ')
-    job_limit = input('Please enter how many jobs do you want to be saved for today: ')
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+def login_linkedin():
+    linkedin_username = input('Please provide linkedin username: ')
+    linkedin_password = input('Please provide linkedin password: ')
+    chrome_options = webdriver.ChromeOptions()
+    #prevent window from closing
+    global driver
+    driver = webdriver.Chrome(executable_path=r"D:\Programming\drivers\chromedriver.exe", chrome_options=chrome_options)
+    chrome_options.add_argument("--incognito")
+    driver.get('https://www.linkedin.com/')
+    driver.maximize_window()
     
-    # to be updated according to linkedins posting filter 
-    job_posting = input('Please enter the time the job was posted: ')
-    job_industry = input('Please provide the industries you are looking for: ')
+    #username field
+    element = driver.find_element(By.ID, 'session_key')
+    element.clear()
+    element.send_keys(linkedin_username)
+
+    #password field
+    element = driver.find_element(By.ID, 'session_password')
+    element.clear()
+    element.send_keys(linkedin_password)
+
+    #login button
+    element = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/button')
+    element.click()
+
+login_linkedin()
 
