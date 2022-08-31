e160dc7a852b021f33eedcb75a664f290135f82a
Suhel <suhelshaikh954@gmail.com>
2022-08-31 01:49:25 -0500

    selenium practice

--- python_practice/selenium/practice_two.py
+++ python_practice/selenium/practice_two.py
@@ -7,13 +7,44 @@ import time
 
 #keeping browser option
 options = webdriver.ChromeOptions()
-options.add_argument("--incognito")
+options.add_argument('--incognito')
 
 #removing the depreciation error
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get('https://demo.nopcommerce.com')
+driver.get('https://www.facebook.com/login/')
 driver.maximize_window()
+
+'''
+CSS Selectors
+
+
+tag and id combo css selector
+syntax: tagname#id input#email
+driver.find_element(By.CSS_SELECTOR, 'input#email').send_keys('test')
+
+tag and class combo
+syntax tagname.class
+driver.find_element(By.CSS_SELECTOR, 'input.inputtext').send_keys('test') #space is there, sometime it may not consider
+driver.find_element(By.CSS_SELECTOR, '.inputtext').send_keys('test')
+
+tag and attribute combo, tag is optional
+syntax tagname[attribut=value]
+driver.find_element(By.CSS_SELECTOR, 'input[name=email]').send_keys('test')
+
+tag, class and attribute combo
+syntax tagname.class[attribute=value]
+driver.find_element(By.CSS_SELECTOR, 'input.inputtext[name=email]').send_keys('test')
+
+'''
+
+
+
+# sliders = driver.find_elements(By.CLASS_NAME, 'homeslider-container')
+# print(len(sliders))
+
+# links=driver.find_elements(By.TAG_NAME, 'a')
+# print(len(links))
 
 # driver.find_element(By.ID, 'small-searchterms').send_keys('Lenovo Thinkpad X1 Carbon Laptop')
 
@@ -21,5 +52,7 @@ driver.maximize_window()
 # driver.find_element(By.LINK_TEXT, 'Register').click()
 
 
+
+
     
-#time.sleep(5)
+time.sleep(5)
